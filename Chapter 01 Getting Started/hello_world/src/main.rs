72e69bbc27c34cb0dacd70707a11c1a1d70e211d
acoost1c <nia.io.nia.work@gmail.com>
2025-06-18 01:26:37 +0700

    First Chapter. I installed the apps, and wrote a first piece of code! Hello, World!
    and git too

---
+++ Chapter 01 Getting Started/hello_world/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // very cool comment!
+    println!("Hello, World!!!");
+    println!("Skibidi toilet :)))))"); // Important skibidi data
+
+    println!("Rust is working.");
+    /*
+    
+    HEHEHEHEHEHahahahhahahah 15.06.25
+
+     */
+
+    
+}
---
+++ Chapter 01 Getting Started/playground/about_me/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // HERE I AM, WITNESS ME AS I TELL YOU ABOUT MYSELF
+    println!("Hello, my name is Nia.");
+    println!("I live in Russia, Kemerovo.");
+    /* print!("I want to eat sand lowkey ngl") */
+    println!("My hobbies are playing videogames, consuming media and maybe sorta cooking.");
+}
